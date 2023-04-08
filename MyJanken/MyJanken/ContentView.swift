73153c35ebaf2a613fc8fce6a8eef4a5f05c8357
kuehar <green95251@gmail.com>
2023-04-08 17:09:21 +0900

    Day1 Lesson3まで完了

--- MyJanken/MyJanken/ContentView.swift
+++ MyJanken/MyJanken/ContentView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answerNumber = 0
     var body: some View {
         VStack {
-            Image("gu")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Text("グー")
+            if answerNumber == 0{
+                Text("これからじゃんけんを始めます")
+            }else if answerNumber == 1{
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("グー")
+            }else if answerNumber == 2{
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+
+                Text("チョキ")
+                    .padding(.bottom)
+            }else{
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+
+                Text("パー")
+                    .padding(.bottom)
+            }
                 
             Button(action: {
-                print("タップされたよ！")
+                answerNumber = Int.random(in: 1...3)
+                
+                var newAnswerNumber = 0
+                
+                repeat{
+                    newAnswerNumber = Int.random(in: 1...3)
+                }while answerNumber == newAnswerNumber
+                
+                answerNumber = newAnswerNumber
+                
             }){
                 Text("じゃんけんをする")
+                    .frame(maxWidth:.infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(Color.pink)
+                    .foregroundColor(Color.white)
             }
         }
 
