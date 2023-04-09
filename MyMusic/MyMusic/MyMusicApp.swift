becd07a26138e1f880ed54ffce59e77154858447
kuehar <green95251@gmail.com>
2023-04-09 15:32:26 +0900

    Initial Commit

---
+++ MyMusic/MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by kuehar on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
