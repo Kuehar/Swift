75195aa16e5d8c3f83b66f8a69d9b85304331de2
kuehar <green95251@gmail.com>
2023-04-09 22:11:14 +0900

    Initial Commit

---
+++ MyMap/MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by kuehar on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
