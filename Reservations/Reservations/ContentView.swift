7a240f967d699a0938e4849d83e36321e6982e11
kuehar <green95251@gmail.com>
2023-04-13 00:17:16 +0900

    Initial Commit

---
+++ Reservations/Reservations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reservations
+//
+//  Created by kuehar on 2023/04/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reservations/Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by kuehar on 2023/04/13.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
