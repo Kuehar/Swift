facbcb21e9069e0eaae7109510d25ed1623cb0f7
kuehar <green95251@gmail.com>
2023-04-12 18:40:28 +0900

    Initial Commit

---
+++ pomodoro_timer/pomodoro_timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pomodoro_timer
+//
+//  Created by kuehar on 2023/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pomodoro_timer/pomodoro_timer/pomodoro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_timerApp.swift
+//  pomodoro_timer
+//
+//  Created by kuehar on 2023/04/12.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
