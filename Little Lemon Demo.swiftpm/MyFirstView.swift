b372960fa0ed80e48b2f70b0c64d4026b1415950
kuehar <green95251@gmail.com>
2023-04-14 00:44:05 +0900

    Finished Observing a TextField

---
+++ Little Lemon Demo.swiftpm/MyFirstView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Little Lemon Demo
+//
+//  Created by kuehar on 2023/04/13.
+//
+
+import Foundation
--- Reservations/Reservations/ContentView.swift
+++ Reservations/Reservations/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var personCount: Int = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Little Lemon")
+            Stepper{
+                Text("Reservation for : \(personCount)")
+            }onIncrement: {
+                personCount += 1
+            }onDecrement: {
+                personCount = (personCount == 1) ? 1 : personCount-1
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(personCount: 10)
     }
 }
